d7b66343881a79d3b44d8efb224fdf94093bd104
catonrug <catonrug.net@gmail.com>
2018-04-14 12:39:39 +0300

    next

--- start.sh
+++ start.sh
@@ -67,49 +67,56 @@ line=$(grep -v "^$" vocabulary.db | tail -$last | sort -R | head -1)
 	if [ $len -gt 0 ]; then
 
 
+		#alternative exit answer
+		echo "end" | grep "$answer" > /dev/null
+		if [ $? -ne 0 ]; then
+		
 		#compare the correct answer with user input
 		echo $correct | grep "$answer" > /dev/null
 
-		#if you typed correct answer and the letter count is equal
-		if [ $? -eq 0 -a ${#correct} -eq ${#answer} ]; then
+			#if you typed correct answer and the letter count is equal
+			if [ $? -eq 0 -a ${#correct} -eq ${#answer} ]; then
 
-		c=$((c+1))
-		togo=$((s-c))
+			c=$((c+1))
+			togo=$((s-c))
 
-		echo correct! $togo more to go
-		
-		#set one more word as known
+			echo correct! $togo more to go
+			
+			#set one more word as known
 
-		#put the word in database (session)
-		echo "$line">> "$tmp"
+			#put the word in database (session)
+			echo "$line">> "$tmp"
 
-		#the word in not coorect. bad news
-		else 
-		echo wrong!
-		sleep 1
-		clear
-		echo
-		echo "   your: $answer"
-		echo
-		echo "correct: $correct"
-		echo 
-		sleep 2
+			#the word in not coorect. bad news
+			else 
+			echo wrong!
+			sleep 1
+			clear
+			echo
+			echo "   your: $answer"
+			echo
+			echo "correct: $correct"
+			echo 
+			sleep 2
 
-			echo "$line" | grep "(.*)" > /dev/null
-			if [ $? -eq 0 ]; then
-			full=$(echo "$ask" | sed "s/\.\.\./$correct/" | sed "s/^.*(//g;s/).*$//g")
-			echo "$full"
+				echo "$line" | grep "(.*)" > /dev/null
+				if [ $? -eq 0 ]; then
+				full=$(echo "$ask" | sed "s/\.\.\./$correct/" | sed "s/^.*(//g;s/).*$//g")
+				echo "$full"
+				fi
+			
+			echo
+			read -p "Press any key to continue... "
+			
+			clear
+			e=$((e+1))
+			echo "$line">> "$todo"
 			fi
+			echo
 		
-		echo
-		read -p "Press any key to continue... "
-		
-		clear
-		e=$((e+1))
-		echo "$line">> "$todo"
+		else
+		c=$((c+1000))
 		fi
-		
-		echo
 	
 	#if the answer was empy string
 	else
@@ -150,14 +157,45 @@ end=$(date +%s)
 
 runtime=$((($(date +%s)-$start)/60)) 
 
-#all words has been parsed
+#if the programm was terminated by user
+echo "end" | grep "$answer" > /dev/null
+if [ $? -ne 0 ]; then
 echo All $s words you have parsed. Number of errors are $e. Total time spent is $runtime minutes. 
+else
+echo
+echo The programm was terminated by user cheat "end"
+echo
+fi
 
 if [ $e -gt 0 ]; then
 echo Words that you need to take time with are:
 #count occurrences of a words who did not work well
 sed "s/^.*|//g" "$todo" | sort | uniq -c | sort -nr
 echo
+#calculate all words wich have more than 1 error. add useless line at the end
+critical=$(sed "s/^.*|//g" "$todo" | sort | uniq -c | grep -v "^[\t ]\+1 " | sed -e '$aend')
+echo Critical words:
+echo "$critical" | sed '$ d'
+
+#lets move the complicated word to the bottom of database
+printf %s "$critical" | while IFS= read -r oneword
+do {
+#remove number
+clean=$(echo "$oneword" | sed "s/^.*[0-9]\+ //")
+
+#search the line in database 
+line=$(grep "|$clean$" vocabulary.db)
+
+#delete appropriate line in file
+sed -i "/$line/d" vocabulary.db
+
+#add the line at the end
+echo "$line" >> vocabulary.db
+
+} done
+
+sed -i '/^$/d' vocabulary.db
+
 fi
 
 if [ -f "$tmp" ]; then
